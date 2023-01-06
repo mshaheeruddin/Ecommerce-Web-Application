7f277ed7f266b2d4ec710aced5c86ff644f330e9
Shaheer <hmuhammadshaheer99@gmail.com>
2023-01-07 02:35:27 +0500

    first commit

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+
+const Navbar = () => {
+  return (
+    <div className="nav">
+        <div className="wrapper">
+            <div className="left"></div>
+            <div className="center"></div>
+            <div className="right"></div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
